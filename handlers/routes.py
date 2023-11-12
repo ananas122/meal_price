748a87f64174b4282bfd2728100ba84dae290ebd
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-11-12 17:16:27 +0100

    To add a new routes for tab poids

--- handlers/routes.py
+++ handlers/routes.py
@@ -1,3 +1,4 @@
+import mysql.connector
 import uuid
 from werkzeug.security import check_password_hash
 from flask import Flask, render_template, redirect, request, jsonify, make_response, session, send_from_directory
@@ -6,8 +7,23 @@ from datetime import datetime
 from dateutil.parser import parse
 import json
 import logging
+import pymysql
 
-api = Api()
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'samira88'
+app.config['MYSQL_DB'] = 'mp_db'
+app.config['MYSQL_CURSORCLASS'] = 'pymysql.cursors.DictCursor'
+
+api = Api(app)
+mysql = pymysql.connect(
+    host=app.config['MYSQL_HOST'],
+    user=app.config['MYSQL_USER'],
+    password=app.config['MYSQL_PASSWORD'],
+    db=app.config['MYSQL_DB'],
+    cursorclass=app.config['MYSQL_CURSORCLASS']
+)
 
 def configure_routes(app):
     @app.route('/set_cookie')
@@ -17,13 +33,11 @@ def configure_routes(app):
         response.set_cookie(app.config["SESSION_COOKIE_NAME"], str(session_id))
         return response
 
-
     @app.route('/clearcookie')
     def clear_cookie():
         resp = make_response("Cookie Cleared")
         resp.set_cookie("my_session_cookie", expires=0)
         return resp
-
 
     @app.route('/add_ingredient', methods=['POST'])
     def add_ingredient():
@@ -33,38 +47,11 @@ def configure_routes(app):
         supplier = request.json.get('supplier')
         unit_cost = request.json.get('unit_cost')
         stock_quantity = request.json.get('stock_quantity')
-
         cur.execute("INSERT INTO ingredients (name, category, supplier, unit_cost, stock_quantity) VALUES (%s, %s, %s, %s, %s)",
                     (name, category, supplier, unit_cost, stock_quantity))
         mysql.connection.commit()
         cur.close()
-
         return 'Ingrédient ajouté avec succès', 201
-
-
-    @app.route('/main_page', methods=['GET', 'POST'])
-    def main_page():
-        if request.method == 'POST':
-            form_name = request.form.get('form_name')
-
-            if form_name == 'signup':
-                try:
-                    # ... code to handle signup requests ...
-                    return 'Utilisateur enregistré avec succès', 201
-                except Exception as e:
-                    print(f"Erreur lors de l'enregistrement de l'utilisateur : {e}")
-                    return 'Erreur lors de l\'enregistrement', 500
-
-            elif form_name == 'login':
-                try:
-                    # ... code to handle login requests ...
-                    return 'Utilisateur connecté avec succès', 200
-                except Exception as e:
-                    print(f"Erreur lors de la connexion de l'utilisateur : {e}")
-                    return 'Erreur lors de la connexion', 500
-
-        return render_template('main_page.html')
-
 
     def get_recipe_from_db(recipe_id):
         cur = mysql.connection.cursor()
@@ -73,57 +60,50 @@ def configure_routes(app):
         cur.close()
         return recipe
 
-
     @app.route('/dashboard')
     def dashboard():
         return render_template('dashboard.html')
-
 
     @app.route('/index1')
     def index1():
         return render_template('index1.html')
 
-
     @app.route('/')
     def index():
         return send_from_directory('.', 'index.html')
-        
+
     @app.route('/recettes')
     def recettes():
         return render_template('recettes.html')
-
 
     @app.route('/calendar')
     def calendar():
         return render_template('calendar.html')
 
-
     @app.route('/stocks')
     def stocks():
         return render_template('stocks.html')
 
-
     @app.route('/ingredients')
     def ingredients():
         return render_template('ingredients.html')
-
+    
+    @app.route('/conversions')
+    def conversions():
+        return render_template('conversions.html')
 
     @app.route('/edit_ingredient', methods=['POST'])
     def edit_ingredient():
         ingredient_id = request.form.get('ingredient_id')
         return redirect('/ingredients')
 
-
     @app.route('/set_selected_recipe', methods=['POST'])
     def set_selected_recipe():
         selected_recipe = request.get_json()
-
         if selected_recipe is None:
             return jsonify({'message': 'Invalid JSON'}), 400
-
         session['selected_recipe'] = json.dumps(selected_recipe, ensure_ascii=False)
         return jsonify({'message': 'Recipe details stored in session'})
-
 
     @app.route("/calculator", methods=["GET", "POST"])
     def calculator():
@@ -133,7 +113,6 @@ def configure_routes(app):
             "instructions": "",
             "quantity_used": ""
         }
-
         if 'selected_recipe' in session:
             recipe_json_string = session.get('selected_recipe')
             recipe_data = json.loads(recipe_json_string)
@@ -141,9 +120,7 @@ def configure_routes(app):
             recipe['ingredients'] = recipe_data.get('ingredients', [])
             recipe['instructions'] = recipe_data.get('instructions', "")
             recipe['quantity_used'] = recipe_data.get('quantity_used', "")
-
         return render_template("calculator.html", recipe=recipe)
-
 
     @app.route('/get_recipe/<int:recipe_id>', methods=['GET'])
     def get_recipe(recipe_id):
@@ -154,56 +131,44 @@ def configure_routes(app):
         }
         return jsonify(recipe_data)
 
-
     @app.route('/api/recipe_details/<int:recipe_id>', methods=['GET'])
     def get_recipe_details(recipe_id):
         cur = mysql.connection.cursor()
         cur.execute("SELECT * FROM recettes WHERE id=%s", [recipe_id])
         recipe = cur.fetchone()
         cur.close()
-
         return jsonify(recipe)
-
 
     @app.route('/select_recipe', methods=['POST'])
     def select_recipe():
         data = request.json
-
         session['selectedRecipe'] = {
             'name': data['name'],
             'ingredients': data['ingredients'],
             'instructions': data['instructions']
         }
-
         return jsonify(success=True)
 
-
     events = []
+
     class Event(Resource):
         def get(self, event_id):
             event = next((event for event in events if event['id'] == event_id), None)
-            if event:
-                return event, 200
-            else:
-                return "Événement non trouvé", 404
+            return (event, 200) if event else ("Événement non trouvé", 404)
 
         def post(self):
             new_event = request.get_json()
             events.append(new_event)
             return new_event, 201
 
-    api.add_resource(Event, "/event", "/event/<string:event_id>")
-
-
+   
     @app.route('/get_events', methods=['GET'])
     def get_events():
         cur = mysql.connection.cursor()
         cur.execute("SELECT * FROM events")
         db_events = cur.fetchall()
         cur.close()
-
         return jsonify(db_events), 200
-
 
     @app.route('/save_event', methods=['POST'])
     def save_event():
@@ -212,15 +177,12 @@ def configure_routes(app):
         title = event_data.get('title')
         start_datetime = event_data.get('start_datetime')
         print(f"Title: {title}, Start Time: {start_datetime}")
-
         try:
             datetime_obj = parse(start_datetime)
         except Exception as e:
             print(f"Erreur lors de la conversion de la date: {e}")
             return 'Format de date invalide', 400
-
         mysql_format = datetime_obj.strftime('%Y-%m-%d %H:%M:%S')
-
         try:
             cur.execute("INSERT INTO events (title, start_datetime) VALUES (%s, %s)",
                         (title, mysql_format))
@@ -228,10 +190,12 @@ def configure_routes(app):
         except Exception as e:
             print(f"Erreur lors de l'insertion dans la base de données: {e}")
             return "Erreur lors de la sauvegarde de l'événement", 500
-
         cur.close()
-
         return 'Événement sauvegardé', 201
 
-        # Register the api with the Flask app
-        api.init_app(app)
+# Register the api with the Flask app
+api.init_app(app)
+configure_routes(app)
+
+if __name__ == '__main__':
+    app.run()
